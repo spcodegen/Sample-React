104e237826ca595a0b0b11c76c4c11b0d557c624
spcodegen <shehansp2000@gmail.com>
2025-05-10 15:03:14 +0530

    Arrow Function,Anonymous Function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import './App.css'
-import BodyContent from './Components/BodyContent/BodyContent'
 import HeaderContent from './Components/HeaderContent/HeaderContent'
 
 function App() {
+
+  function hello01() {
+    console.log("Hello 01");
+  }
+
+  //Arrow Function
+  const hello02 = () => {
+    console.log("Hello 02");
+  }
+
+  const hello03 = (name) => {
+    console.log("Hello 03 " + name);
+  }
+
   return (
     <>
       <div id='wrapper'>
         <HeaderContent />
+        <button onClick={hello01} >Click 01</button>
+        <button onClick={hello02} >Click 02</button>
+        <button onClick={() => { hello03("sandeep"); }} >Click 03</button>
       </div>
     </>
   )
