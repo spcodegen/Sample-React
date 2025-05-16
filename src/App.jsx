dffa9861a35aaaed7f1f874c3d8a4126012cd320
spcodegen <shehansp2000@gmail.com>
2025-05-16 10:17:07 +0530

    React Hooks useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
+import { useState } from 'react'
 import './App.css'
-import BodyContent from './Components/BodyContent/BodyContent'
-import HeaderContent from './Components/HeaderContent/HeaderContent'
-import { Outlet } from 'react-router-dom'
 
 function App() {
 
+  const [count, setcount] = useState(0);
+
+  const decrementFunction = () => {
+    setcount(count - 1);
+  }
+
+  const incrementFunction = () => {
+    setcount(count + 1);
+  }
+
   return (
-     <div id='wrapper'>
-      <HeaderContent />
-      <hr />
-      <Outlet />
+    <div id='wrapper'>
+      <h2>Counter</h2>
+      <button onClick={decrementFunction}>-</button>
+      <button onClick={incrementFunction}>+</button>
+      <br />
+      <p>{count}</p>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
-import ErrorPage from './Components/Pages/ErrorPage.jsx'
-import AboutUs from './Components/Pages/AboutUs.jsx'
-import ContactUs from './Components/Pages/ContactUs.jsx'
-import BlogPage from './Components/Pages/BlogPage.jsx'
-import Home from './Components/Pages/Home.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "about",
-        element: <AboutUs />,
-      },
-      {
-        path: "contact",
-        element: <ContactUs />,
-      },
-      {
-        path: "blog",
-        element: <BlogPage />,
-      },
-    ],
-  },
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>, 
 )
