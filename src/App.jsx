e0698bc6ecf439ccd3fc364f5b4bc96c8ab350cc
spcodegen <shehansp2000@gmail.com>
2025-05-16 15:09:49 +0530

    React Hooks: useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,60 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
 
-  const [count, setcount] = useState(0);
+  const [number, setnumber] = useState(1);
+  const [counter, setcounter] = useState(0);
+  const [clicks, setclicks] = useState(0);
+  const [posts, setposts] = useState([]);
+
+  function handleClick() {
+    setnumber(10);
+  }
 
   const decrementFunction = () => {
-    setcount(count - 1);
+    setcounter(counter - 1);
   }
 
   const incrementFunction = () => {
-    setcount(count + 1);
+    setcounter(counter + 1);
   }
+
+  const additionFunction = () => {
+    setclicks(clicks + 1);
+  }
+
+  useEffect(() => {
+    document.title = `you clicked ${clicks} times`;
+  }, [clicks]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users/1/posts")
+      .then((res) => res.json())
+      .then((blogPosts) => setposts(blogPosts));
+
+
+      console.log("done");
+      
+  },[]);
 
   return (
     <div id='wrapper'>
+      <button onClick={handleClick}>Click</button>
+      <p>{number}</p>
+      <hr />
       <h2>Counter</h2>
       <button onClick={decrementFunction}>-</button>
       <button onClick={incrementFunction}>+</button>
       <br />
-      <p>{count}</p>
+      <p>{counter}</p>
+      <hr />
+      <h4>You Clicked {clicks} times</h4>
+      <button onClick={additionFunction}>Click Me</button>
+      <hr />
+      <ul>
+        {posts && posts.map((post) => <li key={post.id} style={{listStyleType:"circle"}}>{post.title}</li>)}
+      </ul>
     </div>
   )
 }
