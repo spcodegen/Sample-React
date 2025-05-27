a905caa2e67df84b589d471f70fbd4cfbe9a426c
spcodegen <shehansp2000@gmail.com>
2025-05-27 10:39:54 +0530

    Reacr Hooks: useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,52 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import './App.css'
-import Products from "./Components/Products/Products";
+
 function App() {
 
-  const products = [
-    {
-      "id": 1,
-      "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-      "price": 109.95,
-      "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-      "category": "men's clothing",
-      "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-      "rating": {
-        "rate": 3.9,
-        "count": 120
-      }
-    },
-    {
-      "id": 2,
-      "title": "Mens Casual Premium Slim Fit T-Shirts ",
-      "price": 22.3,
-      "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-      "category": "men's clothing",
-      "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-      "rating": {
-        "rate": 4.1,
-        "count": 259
-      }
-    },
-    {
-      "id": 3,
-      "title": "Mens Cotton Jacket",
-      "price": 55.99,
-      "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
-      "category": "men's clothing",
-      "image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-      "rating": {
-        "rate": 4.7,
-        "count": 500
-      }
+  const [input, setinput] = useState("SANDEEP");
+  const [count, setcount] = useState(0);
+
+  const countref = useRef(0);
+  const inputref = useRef();
+  const inputref1 = useRef();
+  const inputref2 = useRef();
+
+  useEffect(() => {
+    // setcount((count) => count + 1);
+    countref.current = countref.current + 1;
+    inputref.current.style.backgroundColor = "red";
+    inputref1.current.style.backgroundColor = "yellow";
+    
+  })
+
+  const handleChange = (e) => {
+    if (e.target.value.length == 0) {
+      setinput("<Input Text goes here>")
+    } else {
+      setinput(e.target.value);
     }
-  ];
+  }
+
+  const handleclick = () => {
+    if (inputref2.current.value == "") {
+      inputref2.current.focus();
+    }else if(inputref2.current.value >50){
+      alert("Success");
+    }else{
+      alert("Danger");
+    }
+  }
 
   return (
     <>
-      {products.map((product) => {
-        return (
+      <input type="text" onChange={handleChange} ref={inputref1}/>
+      <p>You have input {input}</p>
+      <p>Rendered {countref.current}</p>
+      <input type="text" ref={inputref}/>
 
-        // <Products key={product.id} title={product.title} description={product.description} category={product.category} rate={product.rating.rate} />
-
-        <Products key={product.id} {...product} />
-      
-      )
-      })}
+      <br /><br />
+      <input type="text" ref={inputref2}/>
+      <button onClick={handleclick}>Validate</button>
     </>
   )
 }
